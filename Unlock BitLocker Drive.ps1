febf6693ca82a3db2a98b922cbe82051eeea92ec
Dennis Cliffman II <33232470+CliffmanII@users.noreply.github.com>
2025-11-06 09:46:50 -0500

    Update Unlock BitLocker Drive.ps1

--- Unlock BitLocker Drive.ps1
+++ Unlock BitLocker Drive.ps1
@@ -1,3 +1,3 @@
 $InputDrive = Read-Host "Enter assigned letter of drive to unlock (ex: C)"
 $InputString = Read-Host -AsSecureString "Enter key to unlock drive $InputDrive"
-Unlock-BitLocker -MountPoint "E:" -Password $InputString
+Unlock-BitLocker -MountPoint $InputDrive -Password $InputString
