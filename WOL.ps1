975369228863fc50c00f457ca660c03c69bff5c3
Dennis Cliffman II <33232470+CliffmanII@users.noreply.github.com>
2026-07-06 15:50:18 -0400

    Update WOL.ps1
    It now works

--- WOL.ps1
+++ WOL.ps1
@@ -1,7 +1,18 @@
-$Mac = Read-Host -Prompt "Input the target MAC address. Format XXXXXXXXXXXX"
+#Transform MAC into appropriate Byte Array
+$Mac = "XX:XX:XX:XX:XX:XX"
 $MacByteArray = $Mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
 [Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray * 16)
+#Enable Broadcast
+$UdpClient.EnableBroadcast = $true
+#Broadcast WOL packet on port 7
 $UdpClient = New-Object System.Net.Sockets.UdpClient
 $UdpClient.Connect(([System.Net.IPAddress]::Broadcast),7)
-$UdpClient.Send($using:MagicPacket,$using:MagicPacket.Length)
+$UdpClient.Send($MagicPacket,$MagicPacket.Length)
+#Wait then close connection
+Start-Sleep 10
+$UdpClient.Close()
+#Broadcast WOL packet on port 9
+$UdpClient = New-Object System.Net.Sockets.UdpClient
+$UdpClient.Connect(([System.Net.IPAddress]::Broadcast),9)
+$UdpClient.Send($MagicPacket,$MagicPacket.Length)
 $UdpClient.Close()
